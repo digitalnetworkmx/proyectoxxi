6f54bec790d2223da24c56ed9d1fdb6419eea4bd
digitalnetworkmx <47456722+digitalnetworkmx@users.noreply.github.com>
2019-02-08 10:49:25 -0600

    Add files via upload

---
+++ asset/js/firebase.js
@@ -0,0 +1,13 @@
+// Recuerda tener incluido estos js en el archivo html 
+// <script src="https://code.jquery.com/jquery-3.3.1.min.js"></script> 
+// <script src="https://www.gstatic.com/firebasejs/5.8.1/firebase.js"></script>
+// https://github.com/requenahdz/firebase
+var config = {
+    apiKey: "AIzaSyCaU5TKaRincTVldEJ5sFHAaQx16Uz6SQM",
+    authDomain: "fir-8cfd6.firebaseapp.com",
+    databaseURL: "https://fir-8cfd6.firebaseio.com",
+    projectId: "fir-8cfd6",
+    storageBucket: "fir-8cfd6.appspot.com",
+    messagingSenderId: "746254766091"
+ };
+ firebase.initializeApp(config);
